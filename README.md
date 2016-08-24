cdf9950faece953bb98c2dc922d9fc7d7664a782
retamia <retamia@gmail.com>
2016-08-24 17:39:50 +0800

    add example

--- README.md
+++ README.md
@@ -1,3 +1,32 @@
 # cson
 
 A simple json parser for C
+
+## Example
+
+```c
+#include <stdio.h>
+#include <stdlib.h>
+#include "cson.h"
+
+
+int main(){
+    CSON json_obj = parse_cson_with_file("/Users/retamia/Dev/c/cson/test.json");
+    CSON_OBJECT object = json_obj->value.object;
+
+    CSON str_value = (CSON)object->get(object, "str");
+    printf("%s\n", str_value->value.string);
+
+    CSON array_value = (CSON)object->get(object, "strings");
+    CSON_LIST array = array_value->value.array;
+
+    for (unsigned int i = 0; i < array->count; ++i) {
+        CSON item_value = (CSON)array->get(array, i);
+        printf("%s\n", item_value->value.string);
+    }
+
+    return EXIT_SUCCESS;
+}
+
+
+```
--- main.c
+++ main.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "clist.h"
-#include "cmap.h"
 #include "cson.h"
 
 
@@ -9,11 +7,12 @@ int main(){
     CSON json_obj = parse_cson_with_file("/Users/retamia/Dev/c/cson/test.json");
     CSON_OBJECT object = json_obj->value.object;
 
-    CSON str_value = (struct cson*)object->get(object, "str");
+    CSON str_value = (CSON)object->get(object, "str");
     printf("%s\n", str_value->value.string);
 
-    CSON array_value = (struct cson*)object->get(object, "strings");
+    CSON array_value = (CSON)object->get(object, "strings");
     CSON_LIST array = array_value->value.array;
+
     for (unsigned int i = 0; i < array->count; ++i) {
         CSON item_value = (CSON)array->get(array, i);
         printf("%s\n", item_value->value.string);
